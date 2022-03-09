88af1b791b478b916d897a23ad9cdacad4737381
bahstiien <bastien.lecalvez@gmail.com>
2022-03-09 14:26:07 +0100

    add app.use in case of problem

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const http = require("http");
 const express = require("express");
 const mongoose = require("mongoose");
 const WilderModel = require("./models/Wilder");
@@ -20,17 +21,21 @@ app.get("/", (req, res, next) => {
 app.use(express.json());
 
 // CREATE A WILDER
-app.post("/api/wilder/create", wilderController.create);
+app.post("/api/wilders", wilderController.create);
 
 // READ
-app.get("/api/wilder/retrieve", wilderController.retrieveAll);
-app.get("/api/wilder/retrieve/:id", wilderController.findById);
+app.get("/api/wilders", wilderController.retrieveAll);
+app.get("/api/wilders/:id", wilderController.findById);
 
 // UPDATE A WIDLER BY ID
-app.put("/api/wilder/update/:id", wilderController.updateById);
+app.put("/api/wilders/:id", wilderController.updateById);
 
 // DELETE A WILDER BY ID
-app.delete("/api/wilder/delete/:id", wilderController.deleteById);
+app.delete("/api/wilders/:id", wilderController.deleteById);
+
+app.use((req, res, next) => {
+  res.status(404).send("Sorry can't find that!");
+});
 
 //Start Server
 app.listen(3000, () => console.log("Server started on 3000"));
